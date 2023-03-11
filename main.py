afd99436064f71f00653eb08478508e446f87ade
Zeyad Shujoon <zezoo.zezoo2000@gmail.com>
2023-03-11 18:55:32 +0300

    Making changing variables easier and adding clearer instructions

--- main.py
+++ main.py
@@ -5,8 +5,25 @@ from openpyxl import load_workbook
 import arabic_reshaper
 from bidi.algorithm import get_display
 
-# Load the workbook
-wb = load_workbook('6.xlsx')
+
+
+#-----------------------------------------
+#Change these variables to control the font and the text size.
+theFont = 'Arial Unicode'
+theTextSize = 35
+theTextWidth = 1
+#Change these variables to control the color of the text in RGB values.
+theRed = 249
+theGreen = 239
+theBlue = 89
+#Change this variable to control the veritcal position of the text. use as small sample to test the position to find the best position.
+thePosition = 245
+#-----------------------------------------
+
+
+
+# Load the workbook. change the name here if your xlsx file is named diffrently.
+wb = load_workbook('template.xlsx')
 
 # Get the active sheet
 ws = wb.active
@@ -16,13 +33,12 @@ names = [cell.value for cell in ws['A']]
 files = [cell.value for cell in ws['C']]
 
 # Load the image
-img = Image.open('Picture6.jpg')
+img = Image.open('cert.jpg')
 W,H = img.size
 
 # Iterate through the names
 for name, filename in zip(names, files):
     
-    #filename = name
 
     name = arabic_reshaper.reshape(name)
     name = get_display(name)
@@ -33,9 +49,9 @@ for name, filename in zip(names, files):
     # Write the name on the image
     
     draw = ImageDraw.Draw(new_img)
-    font = ImageFont.truetype('Arial Unicode', 35)
+    font = ImageFont.truetype(theFont, theTextSize)
     _, _, w, h = draw.textbbox((0, 0), name, font=font)
-    draw.text(((W-w)/2, 285), name, font=font, stroke_width=1, fill=(53, 87, 80))
+    draw.text(((W-w)/2, thePosition), name, font=font, stroke_width=theTextWidth, fill=(theRed, theGreen, theBlue))
 
-    # Save the new image as a PDF
-    new_img.save(f'6/{filename}.pdf')
+    # Save the new image as a PDF.
+    new_img.save(f'Result/{filename}.pdf')
